22737b277a2f822a134c95865016fd3f9a793c85
alaragil <161920221+alaragil@users.noreply.github.com>
2024-03-02 13:34:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+
+alert("haloo selamat datang, ada hadiah nihh buat kamu");
+prompt(
+  "input nama panggilan yang ayang kamu buat, nanti ada sesuatu yang bakal keluar:)"
+);
+
+alert("yeayy ini bunga buat ayang❤️❤️❤️❤️");
